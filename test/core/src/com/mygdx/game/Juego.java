d6160591487b3052fd5b85f49d3a83950ea3119f
Fausto Gallego <faustogallego666@gmail.com>
2024-09-12 12:47:46 -0300

    mejoras de tilesets

--- test/core/src/com/mygdx/game/Juego.java
+++ test/core/src/com/mygdx/game/Juego.java
@@ -40,67 +40,67 @@ public class Juego extends Game {
 
 	public void render () {
 		super.render();
-//		Gdx.gl.glClearColor(150,14,0,1);
-//		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-//		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-//		
-//		//mover camara con el mouse
-//		if(Gdx.input.isTouched()) {
-//			cam.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
-//			cam.update();
-//		}
-//		
-//		//mover camara con las teclas
-//		if (Gdx.input.isKeyPressed(Keys.UP)) {
-//			cam.translate(0, 1, 0);
-//			cam.update();
-//		}
-//		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
-//			cam.translate(0, -1, 0);
-//			cam.update();
-//		}
-//		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
-//			cam.translate(-1, 0, 0);
-//			cam.update();
-//		}
-//		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
-//			cam.translate(1, 0, 0);
-//			cam.update();
-//		}
-//		
-//		
-//		
-//		//zoom ++
-//		if(Gdx.input.isKeyJustPressed(Keys.O) || Gdx.input.isKeyJustPressed(Keys.NUMPAD_ADD)  ) {
-//			if (cam.zoom <= 0.2f){
-//				cam.update();
-//				cam.zoom = 0.2f;
-//				cam.update();
-//			}
-//			else if (cam.zoom > 0.2f){
-//			cam.zoom = cam.zoom - 0.2f;
-//			cam.update();
-//			}
-//			else if (cam.zoom == 0.0f){
-//				cam.zoom = 0.2f;
-//				cam.update();
-//			}
-//		}
-//		//zoom --
-//		if(Gdx.input.isKeyJustPressed(Keys.P )|| Gdx.input.isKeyJustPressed(Keys.NUMPAD_SUBTRACT)) {
-//			cam.zoom = cam.zoom + 0.2f;
-//			cam.update();
-//		}
-//		
-//		
-//		gameMap.update(Gdx.graphics.getDeltaTime());
-//		gameMap.render(cam, batch);
+		Gdx.gl.glClearColor(150,14,0,1);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+	
+		//mover camara con el mouse
+		if(Gdx.input.isTouched()) {
+			cam.translate(-Gdx.input.getDeltaX(), Gdx.input.getDeltaY());
+			cam.update();
+		}
+		
+		//mover camara con las teclas
+		if (Gdx.input.isKeyPressed(Keys.UP)) {
+			cam.translate(0, 1, 0);
+			cam.update();
+		}
+		if(Gdx.input.isKeyPressed(Keys.DOWN)) {
+			cam.translate(0, -1, 0);
+			cam.update();
+		}
+		if(Gdx.input.isKeyPressed(Keys.LEFT)) {
+			cam.translate(-1, 0, 0);
+			cam.update();
+		}
+		if(Gdx.input.isKeyPressed(Keys.RIGHT)) {
+			cam.translate(1, 0, 0);
+			cam.update();
+		}
+		
+		
+		
+		//zoom ++
+		if(Gdx.input.isKeyJustPressed(Keys.O) || Gdx.input.isKeyJustPressed(Keys.NUMPAD_ADD)  ) {
+			if (cam.zoom <= 0.2f){
+				cam.update();
+				cam.zoom = 0.2f;
+				cam.update();
+			}
+			else if (cam.zoom > 0.2f){
+			cam.zoom = cam.zoom - 0.2f;
+			cam.update();
+			}
+			else if (cam.zoom == 0.0f){
+				cam.zoom = 0.2f;
+				cam.update();
+			}
+		}
+		//zoom --
+		if(Gdx.input.isKeyJustPressed(Keys.P )|| Gdx.input.isKeyJustPressed(Keys.NUMPAD_SUBTRACT)) {
+			cam.zoom = cam.zoom + 0.2f;
+			cam.update();
+		}
+		
+		
+		gameMap.update(Gdx.graphics.getDeltaTime());
+		gameMap.render(cam, batch);
 
 	}
 	
 	@Override
 	public void dispose () {
 		Render.batch.dispose();
-//		gameMap.dispose();
+		gameMap.dispose();
 	}
 }
